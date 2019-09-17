d8606649dfffe194eb4de7af59e33c5b3fcf97c1
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-09-17 16:58:07 +0100

    Initial Commit

---
+++ Check-BankHoliday.ps1
@@ -0,0 +1,8 @@
+Function Check-BankHoliday {
+    Param (
+        [CmdletBinding()]
+        [Parameter(Mandatory)][datetime]$Date
+    )
+    $bankHolidays = (Invoke-RestMethod -Uri "https://www.gov.uk/bank-holidays.json" -Method GET)
+    $bankHolidays.'england-and-wales'.events.date -contains (Get-Date $Date -Format "yyyy-MM-dd")
+}
