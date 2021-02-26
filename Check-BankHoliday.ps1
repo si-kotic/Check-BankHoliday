d99203bcdbc75cc7959a9194f719aa7284cd93c5
Simon Brown <simon.brown@bridgepartners.co.uk>
2021-02-26 20:02:09 +0000

    Added process block

--- Check-BankHoliday.ps1
+++ Check-BankHoliday.ps1
@@ -3,6 +3,8 @@ Function Check-BankHoliday {
         [CmdletBinding()]
         [Parameter(Mandatory,ValueFromPipeline)][datetime]$Date
     )
-    $bankHolidays = (Invoke-RestMethod -Uri "https://www.gov.uk/bank-holidays.json" -Method GET)
-    $bankHolidays.'england-and-wales'.events.date -contains (Get-Date $Date -Format "yyyy-MM-dd")
+    Process {
+        $bankHolidays = (Invoke-RestMethod -Uri "https://www.gov.uk/bank-holidays.json" -Method GET)
+        $bankHolidays.'england-and-wales'.events.date -contains (Get-Date $Date -Format "yyyy-MM-dd")
+    }
 }
